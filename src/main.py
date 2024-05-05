e94bd8b7b81114eb8942a99d61589a2eccee3010
AZhao <2223757448@qq.com>
2024-05-05 13:49:42 +0800

    03. pyqt5_main_window: slot and signal

--- src/main.py
+++ src/main.py
@@ -17,9 +17,38 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('MainWindow Test')
+        # button
         button = QPushButton('Push me')
+        button.setCheckable(True)   # 'True' is like a switch
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
+        # button2
+        self.button2 = QPushButton('Self Signal')
+        self.button2.setCheckable(True)
+        self.button2.clicked.connect(self.the_button_was_released)
+        # button3
+        self.button3 = QPushButton('Update widget status')
+        self.button3.setCheckable(True)
+        self.button3.clicked.connect(self.the_button_click_once)
+        # most widget has available slot
         self.setFixedSize(QSize(400, 300))
         self.setCentralWidget(button)
+        self.setCentralWidget(self.button2)
+        self.setCentralWidget(self.button3)
+    
+    def the_button_was_clicked(self):
+        print('Clicked!')
+    
+    def the_button_was_toggled(self, checked):
+        print('Clicked?', checked)
+
+    def the_button_was_released(self):
+        print('Released?', not self.button2.isChecked())
+    
+    def the_button_click_once(self):
+        self.button3.setText('Already clicked')
+        self.button3.setEnabled(False)
+        self.setWindowTitle('button3 clicked')
 
 def pyqt5_main_window():
     app = QApplication(sys.argv)
